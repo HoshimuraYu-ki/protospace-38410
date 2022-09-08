91b78f8cd97cbd3c4b53aff39c6f773e2dd9925a
HoshimuraYu-ki <111729033+HoshimuraYu-ki@users.noreply.github.com>
2022-09-09 00:51:49 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to "/prototypes/#{@comment.prototype.id}"
+    else
+      @prototype = Prototype.find(@comment.prototype_id)
+      @comments = @prototype.comments.includes(:user)
+      render "prototypes/show"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id,prototype_id: params[:prototype_id])
+  end
+end
